ac98b7ce67e8006213e22da0c658758992e6f9e1
zxwaug <zxwaug@gmail.com>
2025-09-30 10:29:16 +0800

    modified.

--- plotWordcloud.py
+++ plotWordcloud.py
@@ -12,10 +12,10 @@ def generate_wordcloud(text):
     '''
     # 设置显示方式
     d=path.dirname(__file__)
-    alice_mask = np.array(Image.open(path.join(d, "Images//alice_mask.png")))
+    alice_mask = np.array(Image.open(path.join(d, "./Images/xauat.png")))
     font_path=path.join(d,"font//msyh.ttf")
     stopwords = set(STOPWORDS)
-    wc = WordCloud(background_color="white",# 设置背景颜色
+    wc = WordCloud(background_color="#ffffff",# 设置背景颜色
            max_words=2000, # 词云显示的最大词数  
            mask=alice_mask,# 设置背景图片       
            stopwords=stopwords, # 设置停用词
@@ -26,7 +26,7 @@ def generate_wordcloud(text):
     wc.generate(text)
 
     # 生成的词云图像保存到本地
-    wc.to_file(path.join(d, "Images//alice.png"))
+    wc.to_file(path.join(d, "./Images/xauat_result.png"))
 
     # 显示图像
     plt.imshow(wc, interpolation='bilinear')
